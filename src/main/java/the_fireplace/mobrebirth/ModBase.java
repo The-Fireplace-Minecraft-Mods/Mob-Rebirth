82c6a73a09643e69af6df06364e17d69cf6442d4
The_Fireplace <the.f1repl4ce@gmail.com>
2014-11-02 15:53:08 -0600

    changed version

--- src/main/java/the_fireplace/mobrebirth/ModBase.java
+++ src/main/java/the_fireplace/mobrebirth/ModBase.java
@@ -42,7 +42,7 @@ public class ModBase {
 	public static ModBase instance;
 	public static final String MODID = "mobrebirth";
 	public static final String MODNAME = "Mob Rebirth";
-	public static final String VERSION = "1.1.0.1";
+	public static final String VERSION = "1.0.1.0";
 	
 	private static int updateNotification;
 	private static String releaseVersion;
