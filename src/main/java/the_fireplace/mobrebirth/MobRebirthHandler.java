3623de62840bec7285b48c4ed2ccd05b53f1bfb2
The_Fireplace <the.f1repl4ce@gmail.com>
2015-01-09 17:27:36 -0600

    Major Bug Fixes

--- src/main/java/the_fireplace/mobrebirth/MobRebirthHandler.java
+++ src/main/java/the_fireplace/mobrebirth/MobRebirthHandler.java
@@ -1,9 +1,10 @@
 package the_fireplace.mobrebirth;
 
+import the_fireplace.fireplacecore.FireCoreBaseFile;
 import the_fireplace.mobrebirth.config.ConfigValues;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
@@ -18,7 +19,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemMonsterPlacer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.BlockPos;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -27,48 +27,60 @@ import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraft.entity.monster.*;
 
 public class MobRebirthHandler {
+
+public static EntityLivingBase storedEntity;
+public static NBTTagCompound storedNBT;
+public static double storedX;
+public static double storedY;
+public static double storedZ;
 	
-	@SubscribeEvent
-	public void onEntityLivingDeath(LivingDropsEvent event) {
-		if(ConfigValues.NATURALREBIRTH == true){
+@SubscribeEvent
+public void onEntityLivingDeath(LivingDropsEvent event) {
+	if(ConfigValues.NATURALREBIRTH == true){
+		if ((event.entityLiving instanceof IMob)) {//Checks to see if it was a Mob
 			makeMobReborn(event);
+		}else if ((event.entityLiving instanceof IAnimals)) {
+			if (ConfigValues.SPAWNANIMALS == true){
+				makeMobReborn(event);
+			}
 		}
-		else{
-			if(event.source.getEntity() instanceof EntityPlayer){
-				if ((event.entityLiving instanceof IMob)) {//Checks to see if it was a Mob
+	}
+	else{
+		if(event.source.getEntity() instanceof EntityPlayer){
+			if ((event.entityLiving instanceof IMob)) {//Checks to see if it was a Mob
+				makeMobReborn(event);
+			}else if ((event.entityLiving instanceof IAnimals)) {
+				if (ConfigValues.SPAWNANIMALS == true){
 					makeMobReborn(event);
-				}else if ((event.entityLiving instanceof IAnimals)) {//Checks to see if it was an Animal
-					if (ConfigValues.SPAWNANIMALS == true){//Checks if Animal Spawning is enabled
-						makeMobReborn(event);
-					}
 				}
 			}
 		}
 	}
-	private void makeMobReborn(LivingDropsEvent event){
-		double rand = Math.random();
-		EntityLivingBase storedEntity = event.entityLiving;
-		Entity entity;
-		World worldIn = event.entityLiving.worldObj;
-		NBTTagCompound storedData = event.entityLiving.getEntityData();
-		int id = EntityList.getEntityID(event.entityLiving);
-		if (rand <= ConfigValues.SPAWNMOBCHANCE) {
-			if (id > 0 && EntityList.entityEggs.containsKey(id)) {
-					if (ConfigValues.SPAWNMOB == false){
-						ItemStack dropEgg = new ItemStack(Items.spawn_egg, 1, id);
-						event.entityLiving.entityDropItem(dropEgg, 0.0F);}
-					else{
-						entity = EntityList.createEntityByID(id, worldIn);
-		                EntityLiving entityliving = (EntityLiving)entity;
-		                entity.setLocationAndAngles(event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ, MathHelper.wrapAngleTo180_float(worldIn.rand.nextFloat() * 360.0F), 0.0F);
-		                entityliving.rotationYawHead = entityliving.rotationYaw;
-		                entityliving.renderYawOffset = entityliving.rotationYaw;
-		                ((EntityLivingBase) entity).writeToNBT(storedData);
-		                worldIn.spawnEntityInWorld(entity);
-		                }
-				}
-				
+}
+private void makeMobReborn(LivingDropsEvent event){
+	double rand = Math.random();
+	EntityLivingBase storedEntity = event.entityLiving;
+	Entity entity;
+	World worldIn = event.entityLiving.worldObj;
+	NBTTagCompound storedData = event.entityLiving.getEntityData();
+	int id = EntityList.getEntityID(event.entityLiving);
+	if (rand <= ConfigValues.SPAWNMOBCHANCE) {
+		if (id > 0 && EntityList.entityEggs.containsKey(id)) {
+				if (ConfigValues.SPAWNMOB == false){
+					ItemStack dropEgg = new ItemStack(Items.spawn_egg, 1, id);
+					event.entityLiving.entityDropItem(dropEgg, 0.0F);}
+				else{
+					entity = EntityList.createEntityByID(id, worldIn);
+	                EntityLiving entityliving = (EntityLiving)entity;
+	                entity.setLocationAndAngles(event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ, MathHelper.wrapAngleTo180_float(worldIn.rand.nextFloat() * 360.0F), 0.0F);
+	                entityliving.rotationYawHead = entityliving.rotationYaw;
+	                entityliving.renderYawOffset = entityliving.rotationYaw;
+	                ((EntityLivingBase) entity).writeToNBT(storedData);
+	                worldIn.spawnEntityInWorld(entity);
+	                }
+			}
 			
-		}
+		
 	}
+}
 }
