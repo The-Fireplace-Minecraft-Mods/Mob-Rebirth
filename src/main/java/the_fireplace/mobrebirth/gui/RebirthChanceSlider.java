f756b310b81e1c7984a26d780f1bfaf74d205300
The_Fireplace <the.f1repl4ce@gmail.com>
2015-03-23 20:47:30 -0500

    Implemented #8

--- src/main/java/the_fireplace/mobrebirth/gui/RebirthChanceSlider.java
+++ src/main/java/the_fireplace/mobrebirth/gui/RebirthChanceSlider.java
@@ -1,7 +1,11 @@
 package the_fireplace.mobrebirth.gui;
 
+import java.math.RoundingMode;
+
+import java.math.BigDecimal;
 import net.minecraftforge.fml.client.config.GuiConfig;
 import net.minecraftforge.fml.client.config.GuiConfigEntries;
+import net.minecraftforge.fml.client.config.GuiSlider;
 import net.minecraftforge.fml.client.config.GuiConfigEntries.NumberSliderEntry;
 import net.minecraftforge.fml.client.config.IConfigElement;
 
@@ -11,5 +15,20 @@ public class RebirthChanceSlider extends NumberSliderEntry {
 			GuiConfigEntries owningEntryList, IConfigElement configElement) {
 		super(owningScreen, owningEntryList, configElement);
 	}
-
+	@Override
+    public void updateValueButtonText()
+    {
+		//try limiting the number displayed here
+		//this.btnValue.displayString = String.valueOf(round(Double.parseDouble(this.btnValue.displayString), 2));
+        ((GuiSlider) this.btnValue).updateSlider();
+    }
+	
+	public static double round(double value, int places){
+		if(places<0)
+			throw new IllegalArgumentException();
+		
+		BigDecimal bd = new BigDecimal(value);
+		bd = bd.setScale(places, RoundingMode.HALF_UP);
+		return bd.doubleValue();
+	}
 }
