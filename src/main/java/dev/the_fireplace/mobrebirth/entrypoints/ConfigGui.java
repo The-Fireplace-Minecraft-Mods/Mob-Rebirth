8e56a8f6a94d08810c846631a89e2dc559ae237c
The_Fireplace <git@the-fireplace.dev>
2022-03-02 20:52:37 -0600

    Use the ConfigGui entrypoint from FL 6.3

---
+++ src/main/java/dev/the_fireplace/mobrebirth/entrypoints/ConfigGui.java
@@ -0,0 +1,16 @@
+package dev.the_fireplace.mobrebirth.entrypoints;
+
+import com.google.inject.Injector;
+import dev.the_fireplace.lib.api.client.entrypoints.ConfigGuiEntrypoint;
+import dev.the_fireplace.lib.api.client.interfaces.ConfigGuiRegistry;
+import dev.the_fireplace.mobrebirth.config.MRConfigScreenFactory;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public final class ConfigGui implements ConfigGuiEntrypoint {
+    @Override
+    public void registerConfigGuis(Injector injector, ConfigGuiRegistry configGuiRegistry) {
+        configGuiRegistry.register(injector.getInstance(MRConfigScreenFactory.class)::getConfigScreen);
+    }
+}
